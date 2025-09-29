2c6a37e628a724fc97676f24e24ebfd6ec38b2b3
Xandei85 <skriwan.alexander@gmx.de>
2025-09-29 21:50:19 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('schichtplan-v4-6').then(cache => cache.addAll([
-    './','./index.html','./style.css','./app.js','./config.js','./supabaseClient.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
