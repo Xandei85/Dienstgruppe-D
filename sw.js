6b124c70cbc44329b241230e037fcd366aef3abe
Xandei85 <skriwan.alexander@gmx.de>
2025-09-30 11:55:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('schichtplan-v4-6').then(cache => cache.addAll([
+    './','./index.html','./style.css','./app.js','./config.js','./supabaseClient.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./assets/logo.png'
+  ])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
