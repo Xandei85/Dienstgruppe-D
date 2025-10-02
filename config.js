ab283ba77e084361e7fd56a8879d10ac751c4149
Xandei85 <skriwan.alexander@gmx.de>
2025-10-02 21:43:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
-window.APP_CONFIG={PROJECT_NAME:'Dienstgruppe D',NAMES:['Wiesent','Puhl','Botzenhard','Sommer','Schmid']};
+window.APP_CONFIG = {
+  // >>> HIER Supabase Daten eintragen <<<
+  SUPABASE_URL: "https://bvmctyqsydswekyrumzn.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bWN0eXFzeWRzd2VreXJ1bXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNTk5MTEsImV4cCI6MjA3NDczNTkxMX0.GF3JYPn1bYXeXlEjkKwif6iJh7hueDWW1o9nMOmR_98",
+
+  // Projektinfos
+  PROJECT_NAME: "Dienstgruppe D",
+  NAMES: ["Wiesent", "Puhl", "Botzenhard", "Sommer", "Schmid"],
+
+  // Jahres-Einstellungen
+  YEAR_START: 2026,
+  YEAR_END: 2026,
+  START_PATTERN_DATE: "2026-01-02",
+  PATTERN_SHIFT: 0
+};
