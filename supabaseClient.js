78fc7bc08128a465d2ca6f3dcdd68c7ea784df9b
Xandei85 <skriwan.alexander@gmx.de>
2026-02-19 21:58:45 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,19 +1,1 @@
-// supabaseClient.js
 
-(function () {
-  if (!window.supabase || !window.supabase.createClient) {
-    console.warn("Supabase library not loaded.");
-    return;
-  }
-
-  if (!window.SUPABASE_URL || !window.SUPABASE_ANON_KEY) {
-    console.warn("Supabase config missing.");
-    return;
-  }
-
-  // Wichtig: NICHT window.supabase überschreiben!
-  window.supabaseClient = window.supabase.createClient(
-    window.SUPABASE_URL,
-    window.SUPABASE_ANON_KEY
-  );
-})();
